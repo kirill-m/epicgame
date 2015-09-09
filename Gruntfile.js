2f8630b0d8aa4dba1fd9fb27827f81990ab3e494
Kirill Matveev <kmatveev15@gmail.com>
2015-09-09 19:20:05 +0300

    Gruntfile changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,18 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-  concat: { /* Настройки задачи */ },
+  concat: {
+	options: { 
+		separator: ';' },
+	
+	foo: {
+  /* Настройки цели foo */
+  options: { /* ... */ },
+  src: ['js/first.js', 'js/second.js'],
+  dest: 'js/foo.js'
+},
+
+  	bar: { /* Цель bar */ }
+	 },
   any_other_name: 'hello' /* Любое произвольное свойство */
 });
 grunt.loadNpmTasks('grunt-contrib-concat');
